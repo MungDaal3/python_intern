e4d7489b517e04aefdce345cd281142a9c1921be
MungDaal3 <71948379+MungDaal3@users.noreply.github.com>
2020-11-26 23:38:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ app example
 
 """
 import requests
+from requests.exceptions import HTTPError
+
 
 def is_alive_host(hostname: str):
-    response = requests.get("https://www."+hostname)
-    return response.status_code == requests.codes.ok
-
+    try:
+        response = requests.get("https://"+hostname)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        return http_err
+    except Exception:
+        return False
+    else:
+        return True
